9d429f29e0ce54894b0086f778a3de2d9ada4065
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-29 15:31:28 -0500

    adding components, imports to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import ChannelListContainer from './components';
+import ChannelContainer from './components';
+
+const apiKey = '9dx8sq8u6dh3';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-        <h1>Secure Chat Application</h1>
+    <div className='app__wrapper'>
+        <Chat client={client} theme="team light">
+            <ChannelListContainer />
+            <ChannelContainer />
+        </Chat>
     </div>
   )
 }
