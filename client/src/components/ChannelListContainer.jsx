2dd628b42353ee2ff2588f7b7ae03ed57b8bd7dd
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-04-29 16:03:26 -0500

    channellistcontainer component updates, assets

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -4,12 +4,18 @@ import Cookies from 'universal-cookie';
 
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import SecureChatIcon from '../assets/secure-chat.png';
+import LogOutIcon from '../assets/logout.png';
 
 const SideBar = () => (
     <div className='channel-list__sidebar'>
         <div className='channel-list__sidebar__icon1'>
             <div className='icon1__inner'>
                 <img src={SecureChatIcon} alt="Secure Chat" width="30" />
+            </div>
+        </div>
+        <div className='channel-list__sidebar__icon2'>
+            <div className='icon1__inner'>
+                <img src={LogOutIcon} alt="Log Out" width="30" />
             </div>
         </div>
     </div>
