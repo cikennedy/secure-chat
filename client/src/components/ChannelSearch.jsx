6b0aed7d77734fc5e446e2d721d20ae02aa28ec3
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-03 14:39:01 -0500

    implementing channelsearch component inside of the channel list container

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -4,8 +4,38 @@ import { useChatContext } from 'stream-chat-react';
 import { SearchIcon } from '../assets';
 
 const ChannelSearch = () => {
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const getChannels = async (text) => {
+        try {
+            // To-Do: fetch channels 
+        } catch (error) {
+            setQuery('');
+        }
+    }
+
+    const onSearch = (e) => {
+        e.preventDefault();
+
+        setLoading(true);
+        setQuery(e.target.value);
+        getChannels(e.target.value);
+    }
+
   return (
-    <div className='channel-search__'>ChannelSearch</div>
+    <div className='channel-search__container'>
+        <div className='channel-search__input__wrapper'>
+            <div className='channel-search__input__icon'>
+                <SearchIcon />
+            </div>
+            <input className='channel-search__input__text'
+            placeholder='Search' 
+            type='text'
+            value={query}
+            onChange={onSearch}/>
+        </div>
+    </div>
   )
 }
 
