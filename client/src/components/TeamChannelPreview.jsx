b56c3be508ceee172d5f1ceffdb90291ac4237e5
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-03 15:10:40 -0500

    team channel preview component updates for direct messages, previews

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, useChatContext } from 'stream-chat-react';
 
-const TeamChannelPreview = () => {
+const TeamChannelPreview = ({ channel, type }) => {
+    const { channel: activeChannel, client } = useChatContext();
+
+    const ChannelPreview = () => (
+        <p className='channel-preview__item'>
+            # {channel?.data?.name}
+        </p>
+    )
+
+    const DirectPreview = () => {
+        const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
+        
+        return (
+            <div className='channel-preview__item single'>
+                <Avatar 
+                image={members[0]?.user?.image}
+                name={members[0]?.user?.fullName}
+                size={24}
+                />
+                <p>{members[0]?.user?.fullName}</p>
+            </div>
+        )
+    
+    }
+
   return (
     <div>TeamChannelPreview</div>
   )
