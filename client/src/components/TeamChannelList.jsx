459ebfe68b2f175235712f4dd0af66f9bf1ac3d3
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-03 15:01:13 -0500

    channel list component properly exported and completed

--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -2,9 +2,37 @@ import React from 'react'
 
 import { AddChannel } from '../assets';
 
-const TeamChannelList = () => {
+const TeamChannelList = ({ children, error = false, loading, type }) => {
+  if(error) {
+    return type === 'team' ? (
+      <div className='team-channel-list'>
+        <p className='team-channel-list__message'>
+          Connection error, please try again. 
+        </p>
+      </div>
+    ) : null
+  }
+
+  if(loading) {
+    return (
+      <div className='team-channel-list'>
+        <p className='team-channel-list_message loading'>
+          {type === 'team' ? 'Channels' : 'Messages'} loading ... 
+        </p>
+      </div>
+    )
+  }
+
   return (
-    <div>TeamChannelList</div>
+    <div className='team-channel-list'>
+      <div className='team-channel-list__header'>
+        <p className='team-channel-list__header__title'>
+          {type === 'team' ? 'Channels' : 'Direct Messages'}
+        </p>
+        {/* To-Do: button to add channel  */}
+      </div>
+      {children}
+    </div>
   )
 }
 
