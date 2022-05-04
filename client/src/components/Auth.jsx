eac88e4eccf2cdbfe94b9862bada695e3296c6ba
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-04 09:12:45 -0500

    starting to add authentication to application, creating components, installing related packages, rendering on app.jsx

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Cookies from 'universal-cookie';
+import axios from 'axios';
+
+import signUpImage from '../assets/signup.jpeg'
+
+const Auth = () => {
+  return (
+    <div className='auth__form-container'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Auth
