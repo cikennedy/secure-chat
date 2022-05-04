4c6211f868acd342a099dd430fccda7182e1b30b
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-04 11:03:21 -0500

    testing sign in and sign up functionality in the auth component

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -5,13 +5,31 @@ import axios from 'axios';
 import signUpImage from '../assets/signup.jpeg'
 
 const Auth = () => {
-    const [isSignUp, setIsSignUp] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(true);
+
+    const handleChange = () => {
+
+    }
 
   return (
     <div className='auth__form-container'>
         <div className='auth__form-container_fields'>
             <div className='auth__form-container_fields-content'>
                 <p>{isSignUp ? 'Sign Up' : 'Sign In'}</p>
+                <form onSubmit={() => {}}>
+                    {isSignUp && (
+                        <div className='auth__form-container_fields-content_input'>
+                            <label htmlFor='fullName'>Full Name</label>
+                            <input 
+                            name='fullName'
+                            type='text'
+                            placeholder='Full Name'
+                            onChange={handleChange}
+                            required
+                            />
+                        </div>
+                    )}
+                </form>
             </div>
         </div>
     </div>
