e46ad096340b8ff1f8719f4ef04967e92d3936b1
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-13 14:27:19 -0500

    create channel component updates

--- client/src/components/CreateChannel.jsx
+++ client/src/components/CreateChannel.jsx
@@ -4,9 +4,27 @@ import { useChatContext } from 'stream-chat-react';
 import { UserList } from './';
 import { CloseCreateChannel } from '../assets';
 
+const ChannelNameInput = ({ channelName='', setChannelName }) => {
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setChannelName(e.target.value);
+  }
+
+  return (
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input value={channelName} onChange={handleChange} placeholder="channel-name" />
+      <p>Add members</p>
+    </div>
+  )
+}
+
 const CreateChannel = () => {
   return (
-    <div>CreateChannel</div>
+    <div>
+      <ChannelNameInput />
+    </div>
   )
 }
 
