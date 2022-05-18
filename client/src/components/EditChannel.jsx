f6c8495126bd07f566d8546fe96b26bfb9c4cfe7
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-18 16:03:12 -0500

    rendering jsx in the Eidt Channel component, copied the Channel Name Input from the create channel component, passing relevant states and props

--- client/src/components/EditChannel.jsx
+++ client/src/components/EditChannel.jsx
@@ -4,9 +4,37 @@ import { useChatContext } from 'stream-chat-react';
 import { UserList } from './';
 import { CloseCreateChannel } from '../assets';
 
-const EditChannel = () => {
+const ChannelNameInput = ({ channelName='', setChannelName }) => {
+  const { client, setActiveChannel } = useChatContext();
+  const [selectedUsers, setSelectedUsers] = useState([client.userID || '']);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setChannelName(e.target.value);
+  }
+
   return (
-    <div>EditChannel</div>
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+      <input value={channelName} onChange={handleChange} placeholder="channel-name" />
+      <p>Add members</p>
+    </div>
+  )
+}
+
+const EditChannel = ({ setIsEditing }) => {
+  const { channel } = useChatContext();
+  const [channelName, setChannelName] = useState(channel?.data?.name); 
+
+  return (
+    <div className='edit-channel__container'>
+      <div className='edit-channel__header'>
+        <p>Edit Channel</p>
+        <CloseCreateChannel setIsEditing={setIsEditing}/>
+      </div>
+      <ChannelNameInput channelName={channelName} setChannelName={setChannelName} />
+    </div>
   )
 }
 
