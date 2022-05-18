ecfaa6cc83a3e6a12ecc090050a9f3a2cbb3361e
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-05-18 08:41:25 -0500

    template jsx for channel list container component. passing props to the content fragment

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -107,6 +107,18 @@ const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEdi
 
 const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) => {
     const [toggleContainer, setToggleContainer] = useState(false);
+
+    return (
+        <>
+            <div className='channel-list__container'>
+                <ChannelListContent 
+                setIsCreating={setIsCreating}
+                setCreateType={setCreateType}
+                setIsEditing={setIsEditing}
+                />
+            </div>
+        </>
+    )
 }
 
 export default ChannelListContainer
